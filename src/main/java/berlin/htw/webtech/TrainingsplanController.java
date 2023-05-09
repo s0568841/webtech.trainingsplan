b43c9476de24cd2bb87a941d559855da5d5f0aa7
s0568841 <s0568841@htw-berlin.de>
2023-05-09 16:19:18 +0200

    first implementation of the PutController

--- src/main/java/berlin/htw/webtech/TrainingsplanController.java
+++ src/main/java/berlin/htw/webtech/TrainingsplanController.java
@@ -14,11 +14,22 @@ public class TrainingsplanController {
         return service.save(trainingsplan);
     }
 
-
     @GetMapping("/trainingsplan/{id}")
     public Trainingsplan getTrainingsplan (@PathVariable String id){
         Long trainingsplanId = Long.parseLong(id);
         return service.get(trainingsplanId);
+    }
+
+    @PutMapping("/trainingsplan/{id}")
+    public Trainingsplan updateTrainingsplan (@PathVariable String id, @RequestBody Trainingsplan trainingsplan){
+        Trainingsplan updateTrainingsplan = service.get(Long.parseLong(id));
+
+        updateTrainingsplan.setExercise(trainingsplan.getExercise());
+        updateTrainingsplan.setRepetitions(trainingsplan.getRepetitions());
+        updateTrainingsplan.setWeight(trainingsplan.getWeight());
+
+        service.save(updateTrainingsplan);
+        return updateTrainingsplan;
     }
 
 /*testkommentar*/
