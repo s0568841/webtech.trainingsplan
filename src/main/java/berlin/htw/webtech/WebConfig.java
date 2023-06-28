62c724d3afdc91db6c9685861cd5a3bbdc060928
s0568841 <s0568841@htw-berlin.de>
2023-06-28 12:56:34 +0200

    added webconfig for Cors mapping

---
+++ src/main/java/berlin/htw/webtech/WebConfig.java
@@ -0,0 +1,18 @@
+package berlin.htw.webtech;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:5173");
+    }
+}
